870528b5ab743115b43b0e00aaae9d115d3ad4ed
Naim Lasker <naimlasker@live.com>
2018-04-15 21:51:15 +0600

    Angular CDN changed

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,3 @@
 "use strict";
 
 import '../sass/style.scss';
-import angular from 'angular';
-import 'angular-animate/angular-animate';
-import 'angular-aria/angular-aria';
-import 'angular-messages/angular-messages';
-import  '@uirouter/angularjs/release/angular-ui-router';
